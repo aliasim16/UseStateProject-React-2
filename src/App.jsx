ad7bc62ddecb0cbf60082513c42101523a15811e
Ali Asım <aliasim1625@gmail.com>
2023-07-27 23:52:55 +0300

    style eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+
 function App() {
 
 
@@ -33,7 +34,7 @@ function App() {
 
   //is done bilgisi toggle yapılacak
 
-  function handleTask(id ,action="done" ){
+  function handleTask(id ,action ){
     //console.log("id--> ",id,action)
 
     const actionType = action === "done" ? "isDone" : "isDeleted"
@@ -46,7 +47,7 @@ function App() {
           //console.log(item.title)
           setCompleted([item.title,...completed])
         }
-        
+
         else if(item.isDeleted === true){
           setDeleted([item.title,...deleted])
         
@@ -64,88 +65,105 @@ function App() {
   }
 
   return (
-    <>
-      {/* 
-      checkbox: silinenleri göster
-      checkbox: sadece tamamlananları göster is done bilgisi true olanları 
-      input içine yazı yazarken ayni zamanda aramada yapabilsin 
-      uzerine tıklanınca isdone olsun
-      yanda delete butona tıklanınca isdeleted olsun 
-      */}
-    <form onSubmit={handleSubmit} >
+    <div  className='container text-center ' style={{backgroundColor:"#001C30",color:'white',padding:"10px",borderRadius:"10px"}} >
+      <div className="row justify-content-center ">
 
-      <input 
-      type="text" 
-      className='form-control'
-      style={{border:"5px solid orange",width:"50%"}} 
-      name='task' 
-      id='task'
-      onChange={InputHandler}
-      />
-      <button className='btn btn-primary' type='submit' >Kaydet</button>
-      <button className='btn btn-danger' type='reset' >Reset</button>
+          <div className="col align-items-center " >
+            <h1  style={{color:"white"}} >Todo List</h1>
+          {/* 
+          checkbox: silinenleri göster
+          checkbox: sadece tamamlananları göster is done bilgisi true olanları 
+          input içine yazı yazarken ayni zamanda aramada yapabilsin 
+          uzerine tıklanınca isdone olsun
+          yanda delete butona tıklanınca isdeleted olsun 
+          */}
+        <form onSubmit={handleSubmit} >
 
-    </form>
-    
-    
-    {!!tasks.length && <h2>Gorevler:</h2>}
+          <div className='input-group mb-3' >
+          <input 
+          type="text" 
+          className='form-control'
+          
+          name='task' 
+          id='task'
+          onChange={InputHandler}
+          />
+          <button className='btn btn-outline-primary '  type='submit' >Kaydet</button>
+          <button className='btn btn-outline-danger'   type='reset' >Reset</button>
 
-     <ul style={{display:"inline-block"}} >
-     {
-       tasks.map(
-         (item,index) =>
-         <li 
-         key={index}
-         
-         > 
-        {/* {item?.title} {item?.isDone} */}
-        {JSON.stringify(item)}
-          <button onClick={()=> handleTask(index,"done")}  className='btn btn-success'> is done </button>
-          <button onClick={()=> handleTask(index,"delete")}  className='btn btn-danger'> is deleted </button>
-        </li>
-      )
-    }
-    </ul>
+          </div>
 
-    <ul style={{display:"inline-block"}} >
-      {
-        search.filter(item => item.includes(InputValue)).
-        map((item,index) => 
+        </form>
 
-          <li key={index}>
-            {item}
-          </li>
-          )
-      }
-    </ul>
 
-    <ul style={{display:"inline-block"}} >
-        <li>Done olanlar</li>
-        {
-          completed.map((item,index) =>
-            <li key={index}>
-              {item}
+        {!!tasks.length && <h2>Gorevler:</h2>}
+
+          <ul style={{display:"inline-block"}} >
+          {
+            tasks.map(
+              (item,index) =>
+              <li  
+              key={index}
+              style={{listStyleType:"inherit"}}
+              >
+              {JSON.stringify(item)}
+              {/* 
+              {JSON.stringify(item).split(",")[0]}
+              {JSON.stringify(item).split(",")[1]}
+              {JSON.stringify(item).split(",")[2]} */}
+               
+            
+              <button onClick={()=> handleTask(index,"done")}  className='btn btn-outline-primary mx-3   '> is done </button>
+              <button onClick={()=> handleTask(index,"delete")}  className='btn btn-outline-danger'> is deleted </button>
             </li>
           )
-        }
-      
-    </ul>  
-    <ul style={{display:"inline-block"}} >    
-      <li>Deleted Olanlar</li>
-      {
-        deleted.map((item,index) =>
-          <li key={index}>
-            {item}
-          </li>
-        )
-      }
-        
-      
-    </ul>  
+          }
+        </ul>
 
-    </>
+        <ul  style={{display:"inline-block"}} >
+          <li >Arama Sonucu</li>
+          {
+            search.filter(item => item.includes(InputValue)).
+            map((item,index) => 
+
+              <li
+               key={index}
+               className='my-2'
+               >
+                {item}
+              </li>
+              )
+          }
+        </ul>
+
+        <ul style={{display:"inline-block"}} >
+            <li>Done olanlar</li>
+            {
+              completed.map((item,index) =>
+                <li key={index}>
+                  {item}
+                </li>
+              )
+            }
+        </ul>
+
+        <ul style={{display:"inline-block"}} >    
+          <li>Deleted Olanlar</li>
+          {
+            deleted.map((item,index) =>
+              <li key={index}>
+                {item}
+              </li>
+            )
+          }
+            
+          
+        </ul>  
+
+          </div>
+      </div>
+    </div>
   )
 }
-
 
 export default App
